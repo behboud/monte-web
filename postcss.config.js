66a934454d96f0c6738e3f833c3ba049533b8629
Behboud <b.kalantary@gmail.com>
2025-03-17 17:11:00 +0100

    WIP

--- postcss.config.js
+++ postcss.config.js
@@ -1,29 +1,12 @@
+const franken = require('franken-ui/shadcn-ui/preset-quick');
 
 module.exports = {
-    plugins: {
-        'postcss-import': {},
-        tailwindcss: {
-            files: [
-                "hugo_stats.json",
-            ],
-            content: ["content/**/*.md", "layouts/**/*.html"],
-            theme: {
-                extend: {
-                    colors: {
-                        primary: "#121212",
-                        body: "#fff",
-                        light: "#fcfaf7",
-                        monte: "#222477",
-                        bgLight: "#fcfaf7",
-                    },
-                    fontFamily: {
-                        calligraphy: ["italic", "Zapfino"],
-                        sans: ["Overpass"],
-                    },
-                },
-            },
-            plugins: [],
-        },
-        autoprefixer: {},
-    }
+    plugins: [
+        require('postcss-import'),
+        require('tailwindcss'),
+        require('franken-ui/postcss/combine-duplicated-selectors')({
+            removeDuplicatedProperties: true
+        }),
+        require('autoprefixer')
+    ]
 }
