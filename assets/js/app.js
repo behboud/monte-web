bf60ef21153dcdc2c75a83956ccea38cd990910d
behboud kalantary <b.kalantary@gmail.com>
2025-03-24 18:15:42 +0100

    fix: cms not loaded to avoid peer dependency errors

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,4 @@
 // app script that can bundle
 import "franken-ui/js/core.iife";
 import "franken-ui/js/icon.iife";
-import CMS from 'decap-cms-app';
-import { de } from 'decap-cms-locales';
-
-CMS.registerLocale('de', de);
+import "mmenu-js/dist/mmenu.js";
