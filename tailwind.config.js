6b65251bdd52c05fb9f637668ecfc4225ff93bf5
behboud kalantary <b.kalantary@gmail.com>
2025-05-29 16:42:57 +0200

    fix: font issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,44 +7,39 @@ https://marketplace.visualstudio.com/items?itemName=bradlc.vscode-tailwindcss
 The rest of this file is intentionally empty.
 */
 
-import franken from 'franken-ui/shadcn-ui/preset-quick';
+import franken from "franken-ui/shadcn-ui/preset-quick";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	
-    presets: [franken],
-    files: [
-        "hugo_stats.json",
-    ],
-    content: ["content/**/*.md", "layouts/**/*.html"],
-    theme: {
-        extend: {
-            colors: {
-                primary: "#121212",
-                body: "#fff",
-                light: "#fcfaf7",
-                monte: "#222477",
-                foreground: "#222477",
-                bgLight: "#fcfaf7",
-            },
-            fontFamily: {
-                calligraphy: ["italic", "Zapfino"],
-                sans: ["Overpass"],
-            },
-        },
+  presets: [franken],
+  files: ["hugo_stats.json"],
+  content: ["content/**/*.md", "layouts/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#121212",
+        body: "#fff",
+        light: "#fcfaf7",
+        monte: "#222477",
+        foreground: "#222477",
+        bgLight: "#fcfaf7",
+      },
+      fontFamily: {
+        calligraphy: ["Tangerine", "cursive"],
+        sans: ["Overpass", "sans-serif"],
+      },
     },
-    safelist: [
-        {
-            pattern: /^uk-/
-        },
-        'ProseMirror',
-        'ProseMirror-focused',
-        'tiptap',
-        'mr-2',
-        'mt-2',
-        'opacity-50'
-    ],
-    plugins: [
-
-    ],
+  },
+  safelist: [
+    {
+      pattern: /^uk-/,
+    },
+    "ProseMirror",
+    "ProseMirror-focused",
+    "tiptap",
+    "mr-2",
+    "mt-2",
+    "opacity-50",
+  ],
+  plugins: [],
 };
